e080718a2722cb5bc985dd832f256ca33daf5dba
PrOF-kk <valerio.colella.2001@gmail.com>
2021-04-08 22:51:00 +0200

    Darken default green for comments - string literals - char literals

--- mars/venus/editors/jeditsyntax/SyntaxUtilities.java
+++ mars/venus/editors/jeditsyntax/SyntaxUtilities.java
@@ -97,13 +97,13 @@
       // SyntaxStyle constructor params: color, italic?, bold?
       // All need to be assigned even if not used by language (no gaps in array)
          styles[Token.NULL]     = new SyntaxStyle(Color.black,false,false);
-         styles[Token.COMMENT1] = new SyntaxStyle(new Color(0x00CC33),true,false);//(Color.black,true,false);
+         styles[Token.COMMENT1] = new SyntaxStyle(new Color(0x00961e),true,false);//(Color.black,true,false);
          styles[Token.COMMENT2] = new SyntaxStyle(new Color(0x990033),true,false);
          styles[Token.KEYWORD1] = new SyntaxStyle(Color.blue,false,false);//(Color.black,false,true);
          styles[Token.KEYWORD2] = new SyntaxStyle(Color.magenta,false,false);
          styles[Token.KEYWORD3] = new SyntaxStyle(Color.red,false,false);//(new Color(0x009600),false,false);
-         styles[Token.LITERAL1] = new SyntaxStyle(new Color(0x00CC33),false,false);//(new Color(0x650099),false,false);
-         styles[Token.LITERAL2] = new SyntaxStyle(new Color(0x00CC33),false,false);//(new Color(0x650099),false,true);
+         styles[Token.LITERAL1] = new SyntaxStyle(new Color(0x00961e),false,false);//(new Color(0x650099),false,false);
+         styles[Token.LITERAL2] = new SyntaxStyle(new Color(0x00961e),false,false);//(new Color(0x650099),false,true);
          styles[Token.LABEL]    = new SyntaxStyle(Color.black,true,false);//(new Color(0x990033),false,true);
          styles[Token.OPERATOR] = new SyntaxStyle(Color.black,false,true);
          styles[Token.INVALID]  = new SyntaxStyle(Color.red,false,false);
