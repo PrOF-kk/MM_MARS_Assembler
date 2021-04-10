96617019083c9e2ff9c84ea85cc278e60ee5e476
PrOF-kk <valerio.colella.2001@gmail.com>
2021-04-10 19:45:15 +0200

    Tweak and add Colibre to About message

--- mars/venus/HelpAboutAction.java
+++ mars/venus/HelpAboutAction.java
@@ -42,14 +42,16 @@ public class HelpAboutAction extends GuiAction {
 
 	public void actionPerformed(ActionEvent e) {
 		JOptionPane.showMessageDialog(mainUI,
-				"MARS " + Globals.version
-				+ "    Copyright " + Globals.copyrightYears + "\n" + Globals.copyrightHolders + "\n"
-				+ "MARS is the Mips Assembler and Runtime Simulator.\n\n"
-				+ "Mars image courtesy of NASA/JPL.\n" + "Toolbar and menu icons are from:\n"
-				+ "  *  Tango Desktop Project (tango.freedesktop.org),\n"
-				+ "  *  glyFX (www.glyfx.com) Common Toolbar Set,\n"
-				+ "  *  KDE-Look (www.kde-look.org) crystalline-blue-0.1,\n"
-				+ "  *  Icon-King (www.icon-king.com) Nuvola 1.0.\n"
+				"MARS - Mips Assembler and Runtime Simulator\n"
+				+ "Version " + Globals.version + "\n"
+				+ "Copyright " + Globals.copyrightYears + Globals.copyrightHolders + "\n\n"
+				+ "Mars image courtesy of NASA/JPL.\n\n"
+				+ "Toolbar and menu icons are from the Colibre icon pack\n\n"
+				+ "Tool and additional icons are from:\n"
+				+ " • Tango Desktop Project,\n"
+				+ " • glyFX Common Toolbar Set,\n"
+				+ " • KDE-Look crystalline-blue-0.1,\n"
+				+ " • Icon-King Nuvola 1.0\n\n"
 				+ "Print feature adapted from HardcopyWriter class in David Flanagan's\n"
 				+ "Java Examples in a Nutshell 3rd Edition, O'Reilly, ISBN 0-596-00620-9.",
 				"About Mars", JOptionPane.INFORMATION_MESSAGE, new ImageIcon("images/RedMars50.png"));
