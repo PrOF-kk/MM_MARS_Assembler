120459f91c6b83365f9d139938927567c39c57ec
PrOF-kk <valerio.colella.2001@gmail.com>
2021-05-24 17:13:53 +0200

    SystemIO: Cleanup

--- mars/util/SystemIO.java
+++ mars/util/SystemIO.java
@@ -47,7 +47,7 @@ public class SystemIO {
 	/** Maximum number of files that can be open */
 	public static final int SYSCALL_MAXFILES = 32;
 	/** String used for description of file error */
-	public static String fileErrorString = new String("File operation OK");
+	public static String fileErrorString = "File operation OK";
 
 	private static final int O_RDONLY = 0x00000000;
 	private static final int O_WRONLY = 0x00000001;
@@ -131,7 +131,7 @@ public class SystemIO {
 	 * Client is responsible for catching NumberFormatException.
 	 * @param serviceNumber the number assigned to Read Double syscall (default 7)
 	 * @return double value corresponding to user input
-	 * 1 Aug 2005 DPS, based on Ken Vollmar's readFloat
+	 * @version 1 Aug 2005 DPS, based on Ken Vollmar's readFloat
 	 */
 	public static double readDouble(int serviceNumber) {
 		String input = "0";
@@ -266,7 +266,7 @@ public class SystemIO {
 
 		if (!FileIOData.fdInUse(fd, 1)) // Check the existence of the "write" fd
 		{
-			fileErrorString = new String("File descriptor " + fd + " is not open for writing");
+			fileErrorString = "File descriptor " + fd + " is not open for writing";
 			return -1;
 		}
 		// retrieve FileOutputStream from storage
@@ -290,11 +290,11 @@ public class SystemIO {
 			outputStream.flush();// DPS 7-Jan-2013
 		}
 		catch (IOException e) {
-			fileErrorString = new String("IO Exception on write of file with fd " + fd);
+			fileErrorString = "IO Exception on write of file with fd " + fd;
 			return -1;
 		}
 		catch (IndexOutOfBoundsException e) {
-			fileErrorString = new String("IndexOutOfBoundsException on write of file with fd" + fd);
+			fileErrorString = "IndexOutOfBoundsException on write of file with fd" + fd;
 			return -1;
 		}
 
@@ -316,7 +316,7 @@ public class SystemIO {
 		if (fd == STDIN && Globals.getGui() != null) {
 			String input = Globals.getGui().getMessagesPane().getInputString(lengthRequested);
 			byte[] bytesRead = input.getBytes();
-			;
+
 			for (int i = 0; i < myBuffer.length; i++) {
 				myBuffer[i] = (i < bytesRead.length) ? bytesRead[i] : 0;
 			}
@@ -327,14 +327,14 @@ public class SystemIO {
 
 		if (!FileIOData.fdInUse(fd, 0)) // Check the existence of the "read" fd
 		{
-			fileErrorString = new String("File descriptor " + fd + " is not open for reading");
+			fileErrorString = "File descriptor " + fd + " is not open for reading";
 			return -1;
 		}
 		// retrieve FileInputStream from storage
-		InputStream InputStream = (InputStream) FileIOData.getStreamInUse(fd);
+		InputStream inputStream = (InputStream) FileIOData.getStreamInUse(fd);
 		try {
 			// Reads up to lengthRequested bytes of data from this Input stream into an array of bytes.
-			retValue = InputStream.read(myBuffer, 0, lengthRequested);
+			retValue = inputStream.read(myBuffer, 0, lengthRequested);
 			// This method will return -1 upon EOF, but our spec says that negative
 			// value represents an error, so we return 0 for EOF.  DPS 10-July-2008.
 			if (retValue == -1) {
@@ -342,11 +342,11 @@ public class SystemIO {
 			}
 		}
 		catch (IOException e) {
-			fileErrorString = new String("IO Exception on read of file with fd " + fd);
+			fileErrorString = "IO Exception on read of file with fd " + fd;
 			return -1;
 		}
 		catch (IndexOutOfBoundsException e) {
-			fileErrorString = new String("IndexOutOfBoundsException on read of file with fd" + fd);
+			fileErrorString = "IndexOutOfBoundsException on read of file with fd" + fd;
 			return -1;
 		}
 		return retValue;
@@ -388,7 +388,7 @@ public class SystemIO {
 				FileIOData.setStreamInUse(fdToUse, inputStream); // Save stream for later use
 			}
 			catch (FileNotFoundException e) {
-				fileErrorString = new String("File " + filename + " not found, open for input.");
+				fileErrorString = "File " + filename + " not found, open for input.";
 				retValue = -1;
 			}
 		}
@@ -400,7 +400,7 @@ public class SystemIO {
 				FileIOData.setStreamInUse(fdToUse, outputStream); // Save stream for later use
 			}
 			catch (FileNotFoundException e) {
-				fileErrorString = new String("File " + filename + " not found, open for output.");
+				fileErrorString = "File " + filename + " not found, open for output.";
 				retValue = -1;
 			}
 		}
@@ -443,6 +443,10 @@ public class SystemIO {
 			inputReader = new BufferedReader(new InputStreamReader(System.in));
 		}
 		return inputReader;
+	}
+	
+	private SystemIO() {
+		// Hide implicit public constructor
 	}
 
 	// //////////////////////////////////////////////////////////////////////////////
@@ -553,13 +557,13 @@ public class SystemIO {
 		private static int nowOpening(String filename, int flag) {
 			int i = 0;
 			if (filenameInUse(filename)) {
-				fileErrorString = new String("File name " + filename + " is already open.");
+				fileErrorString = "File name " + filename + " is already open.";
 				return -1;
 			}
 
 			if (flag != O_RDONLY && flag != O_WRONLY && flag != (O_WRONLY | O_APPEND)) // Only read and write are implemented
 			{
-				fileErrorString = new String("File name " + filename + " has unknown requested opening flag");
+				fileErrorString = "File name " + filename + " has unknown requested opening flag";
 				return -1;
 			}
 
@@ -569,15 +573,14 @@ public class SystemIO {
 
 			if (i >= SYSCALL_MAXFILES) // no available file descriptors
 			{
-				fileErrorString = new String(
-						"File name " + filename + " exceeds maximum open file limit of " + SYSCALL_MAXFILES);
+				fileErrorString = "File name " + filename + " exceeds maximum open file limit of " + SYSCALL_MAXFILES;
 				return -1;
 			}
 
 			// Must be OK -- put filename in table
-			fileNames[i] = new String(filename); // our table has its own copy of filename
+			fileNames[i] = filename; // our table has its own copy of filename
 			fileFlags[i] = flag;
-			fileErrorString = new String("File operation OK");
+			fileErrorString = "File operation OK";
 			return i;
 
 		}
