d9057c364f9d38f10a4ca9ab834ee79d41ff08ad
PrOF-kk <valerio.colella.2001@gmail.com>
2021-05-24 16:40:34 +0200

    DumpFormatLoader: Code refactor

--- mars/mips/dump/DumpFormatLoader.java
+++ mars/mips/dump/DumpFormatLoader.java
@@ -1,6 +1,5 @@
 package mars.mips.dump;
 
-import mars.*;
 import mars.util.*;
 import java.util.*;
 import java.lang.reflect.*;
@@ -48,7 +47,7 @@ public class DumpFormatLoader {
 	private static final String SYSCALL_INTERFACE = "DumpFormat.class";
 	private static final String CLASS_EXTENSION = "class";
 
-	private static ArrayList formatList = null;
+	private static ArrayList<DumpFormat> formatList = null;
 
 	/**
 	 * Dynamically loads dump formats into an ArrayList. This method is adapted from
@@ -56,20 +55,20 @@ public class DumpFormatLoader {
 	 * (bret@hypefiend.com) is co-author of the book "Developing Games in Java".
 	 * Also see the ToolLoader and SyscallLoader classes elsewhere in MARS.
 	 */
-	public ArrayList loadDumpFormats() {
+	public ArrayList<DumpFormat> loadDumpFormats() {
 		// The list will be populated only the first time this method is called.
 		if (formatList == null) {
-			formatList = new ArrayList();
+			formatList = new ArrayList<>();
 			// grab all class files in the dump directory
-			ArrayList candidates = FilenameFinder.getFilenameList(this.getClass().getClassLoader(), DUMP_DIRECTORY_PATH, CLASS_EXTENSION);
+			ArrayList<String> candidates = FilenameFinder.getFilenameList(this.getClass().getClassLoader(), DUMP_DIRECTORY_PATH, CLASS_EXTENSION);
 			for (int i = 0; i < candidates.size(); i++) {
-				String file = (String) candidates.get(i);
+				String file = candidates.get(i);
 				try {
 					// grab the class, make sure it implements DumpFormat, instantiate, add to list
 					String formatClassName = CLASS_PREFIX + file.substring(0, file.indexOf(CLASS_EXTENSION) - 1);
 					Class clas = Class.forName(formatClassName);
 					if (DumpFormat.class.isAssignableFrom(clas) && !Modifier.isAbstract(clas.getModifiers()) && !Modifier.isInterface(clas.getModifiers())) {
-						formatList.add(clas.newInstance());
+						formatList.add((DumpFormat) clas.getDeclaredConstructor().newInstance());
 					}
 				}
 				catch (Exception e) {
@@ -80,11 +79,11 @@ public class DumpFormatLoader {
 		return formatList;
 	}
 
-	public static DumpFormat findDumpFormatGivenCommandDescriptor(ArrayList formatList, String formatCommandDescriptor) {
+	public static DumpFormat findDumpFormatGivenCommandDescriptor(ArrayList<DumpFormat> formatList, String formatCommandDescriptor) {
 		DumpFormat match = null;
 		for (int i = 0; i < formatList.size(); i++) {
-			if (((DumpFormat) formatList.get(i)).getCommandDescriptor().equals(formatCommandDescriptor)) {
-				match = (DumpFormat) formatList.get(i);
+			if (formatList.get(i).getCommandDescriptor().equals(formatCommandDescriptor)) {
+				match = formatList.get(i);
 				break;
 			}
 		}
