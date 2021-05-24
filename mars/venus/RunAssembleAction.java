0d612a50264ebb98e8ef08d82d2c510633099bbd
PrOF-kk <valerio.colella.2001@gmail.com>
2021-05-24 15:48:41 +0200

    RunAssembleAction: Style refactor

--- mars/venus/RunAssembleAction.java
+++ mars/venus/RunAssembleAction.java
@@ -1,4 +1,5 @@
 package mars.venus;
+
 import mars.*;
 import mars.util.*;
 import mars.mips.hardware.*;
@@ -47,8 +48,7 @@ public class RunAssembleAction extends GuiAction {
 	// Threshold for adding filename to printed message of files being assembled.
 	private static final int LINE_LENGTH_LIMIT = 60;
 
-	public RunAssembleAction(String name, Icon icon, String descrip,
-			Integer mnemonic, KeyStroke accel, VenusUI gui) {
+	public RunAssembleAction(String name, Icon icon, String descrip, Integer mnemonic, KeyStroke accel, VenusUI gui) {
 		super(name, icon, descrip, mnemonic, accel, gui);
 	}
 
@@ -56,6 +56,7 @@ public class RunAssembleAction extends GuiAction {
 	static ArrayList getMIPSprogramsToAssemble() {
 		return MIPSprogramsToAssemble;
 	}
+
 	static boolean getExtendedAssemblerEnabled() {
 		return extendedAssemblerEnabled;
 	}
@@ -71,37 +72,41 @@ public class RunAssembleAction extends GuiAction {
 		RegistersPane registersPane = mainUI.getRegistersPane();
 		extendedAssemblerEnabled = Globals.getSettings().getExtendedAssemblerEnabled();
 		warningsAreErrors = Globals.getSettings().getWarningsAreErrors();
-		if(FileStatus.getFile()!= null){  
+		if (FileStatus.getFile() != null) {
 			if (FileStatus.get() == FileStatus.EDITED) {
 				mainUI.editor.save();
 			}
-			try{
+			try {
 				Globals.program = new MIPSprogram();
 				ArrayList filesToAssemble;
-				if (Globals.getSettings().getAssembleAllEnabled()) {// setting calls for multiple file assembly 
+				if (Globals.getSettings().getAssembleAllEnabled()) {// setting calls for multiple file assembly
 					filesToAssemble = FilenameFinder.getFilenameList(
-							new File(FileStatus.getName()).getParent(), Globals.fileExtensions);
-				} 
+							new File(FileStatus.getName()).getParent(),
+							Globals.fileExtensions);
+				}
 				else {
 					filesToAssemble = new ArrayList();
-					filesToAssemble.add(FileStatus.getName());  
+					filesToAssemble.add(FileStatus.getName());
 				}
 				String exceptionHandler = null;
-				if (Globals.getSettings().getExceptionHandlerEnabled() &&
-						Globals.getSettings().getExceptionHandler() != null &&
-						Globals.getSettings().getExceptionHandler().length() > 0) {
+				if (Globals.getSettings().getExceptionHandlerEnabled()
+						&& Globals.getSettings().getExceptionHandler() != null
+						&& Globals.getSettings().getExceptionHandler().length() > 0) {
+					
 					exceptionHandler = Globals.getSettings().getExceptionHandler();
 				}
-				MIPSprogramsToAssemble = Globals.program.prepareFilesForAssembly(filesToAssemble, FileStatus.getFile().getPath(), exceptionHandler);					
-				mainUI.messagesPane.postMarsMessage(buildFileNameList(name+": assembling ", MIPSprogramsToAssemble));
+				
+				MIPSprogramsToAssemble = Globals.program.prepareFilesForAssembly(
+						filesToAssemble, FileStatus.getFile().getPath(), exceptionHandler);
+				
+				mainUI.messagesPane.postMarsMessage(buildFileNameList(name + ": assembling ", MIPSprogramsToAssemble));
 				// added logic to receive any warnings and output them.... DPS 11/28/06
 				ErrorList warnings = Globals.program.assemble(MIPSprogramsToAssemble, extendedAssemblerEnabled,
 						warningsAreErrors);
 				if (warnings.warningsOccurred()) {
 					mainUI.messagesPane.postMarsMessage(warnings.generateWarningReport());
 				}
-				mainUI.messagesPane.postMarsMessage(
-						name+": operation completed successfully.\n\n");
+				mainUI.messagesPane.postMarsMessage(name + ": operation completed successfully.\n\n");
 				FileStatus.setAssembled(true);
 				FileStatus.set(FileStatus.RUNNABLE);
 				RegisterFile.resetRegisters();
@@ -109,7 +114,7 @@ public class RunAssembleAction extends GuiAction {
 				Coprocessor0.resetRegisters();
 				executePane.getTextSegmentWindow().setupTable();
 				executePane.getDataSegmentWindow().setupTable();
-				executePane.getDataSegmentWindow().highlightCellForAddress(Memory.dataBaseAddress); 
+				executePane.getDataSegmentWindow().highlightCellForAddress(Memory.dataBaseAddress);
 				executePane.getDataSegmentWindow().clearHighlighting();
 				executePane.getLabelsWindow().setupTable();
 				executePane.getTextSegmentWindow().setCodeHighlighting(true);
@@ -122,28 +127,30 @@ public class RunAssembleAction extends GuiAction {
 				mainUI.getMainPane().setSelectedComponent(executePane);
 
 				// Aug. 24, 2005 Ken Vollmar
-				SystemIO.resetFiles( );  // Ensure that I/O "file descriptors" are initialized for a new program run
+				SystemIO.resetFiles(); // Ensure that I/O "file descriptors" are initialized for a new program run
 
 			}
 			catch (ProcessingException pe) {
 				String errorReport = pe.errors().generateErrorAndWarningReport();
 				mainUI.messagesPane.postMarsMessage(errorReport);
-				mainUI.messagesPane.postMarsMessage(
-						name+": operation completed with errors.\n\n");
+				mainUI.messagesPane.postMarsMessage(name + ": operation completed with errors.\n\n");
 				// Select editor line containing first error, and corresponding error message.
 				ArrayList errorMessages = pe.errors().getErrorMessages();
-				for (int i=0; i<errorMessages.size(); i++) {
+				for (int i = 0; i < errorMessages.size(); i++) {
 					ErrorMessage em = (ErrorMessage) errorMessages.get(i);
-					// No line or position may mean File Not Found (e.g. exception file). Don't try to open. DPS 3-Oct-2010
-					if (em.getLine()==0 && em.getPosition()==0) {
+					// No line or position may mean File Not Found (e.g. exception file). Don't try
+					// to open. DPS 3-Oct-2010
+					if (em.getLine() == 0 && em.getPosition() == 0) {
 						continue;
 					}
-					if (!em.isWarning() || warningsAreErrors) { 
+					if (!em.isWarning() || warningsAreErrors) {
 						Globals.getGui().getMessagesPane().selectErrorMessage(em.getFilename(), em.getLine(), em.getPosition());
-						// Bug workaround: Line selection does not work correctly for the JEditTextArea editor
-						// when the file is opened then automatically assembled (assemble-on-open setting).
-						// Automatic assemble happens in EditTabbedPane's openFile() method, by invoking
-						// this method (actionPerformed) explicitly with null argument.  Thus e!=null test.
+						// Bug workaround: Line selection does not work correctly
+						// for the JEditTextArea editor when the file is opened
+						// then automatically assembled (assemble-on-open setting).
+						// Automatic assemble happens in EditTabbedPane's openFile(
+						// method, by invoking this method (actionPerformed) explicitly
+						// with null argument. Thus e != null test.
 						// DPS 9-Aug-2010
 						if (e != null) {
 							Globals.getGui().getMessagesPane().selectEditorTextLine(em.getFilename(), em.getLine(), em.getPosition());
@@ -154,7 +161,7 @@ public class RunAssembleAction extends GuiAction {
 				FileStatus.setAssembled(false);
 				FileStatus.set(FileStatus.NOT_EDITED);
 			}
-		}               
+		}
 	}
 
 	// Handy little utility for building comma-separated list of filenames
@@ -162,15 +169,15 @@ public class RunAssembleAction extends GuiAction {
 	private String buildFileNameList(String preamble, ArrayList programList) {
 		String result = preamble;
 		int lineLength = result.length();
-		for (int i=0; i<programList.size(); i++) {
-			String filename = ((MIPSprogram)programList.get(i)).getFilename();
-			result += filename + ((i<programList.size()-1)?", ":"");
+		for (int i = 0; i < programList.size(); i++) {
+			String filename = ((MIPSprogram) programList.get(i)).getFilename();
+			result += filename + ((i < programList.size() - 1) ? ", " : "");
 			lineLength += filename.length();
 			if (lineLength > LINE_LENGTH_LIMIT) {
 				result += "\n";
 				lineLength = 0;
 			}
 		}
-		return result + ((lineLength==0)?"":"\n") + "\n";
+		return result + ((lineLength == 0) ? "" : "\n") + "\n";
 	}
-}        
+}
