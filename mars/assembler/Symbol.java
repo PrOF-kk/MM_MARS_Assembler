d6dfeb54270c9652b130e060b1770db119bc30e9
PrOF-kk <valerio.colella.2001@gmail.com>
2021-06-05 14:39:39 +0200

    Symbol: Minor cleanup

--- mars/assembler/Symbol.java
+++ mars/assembler/Symbol.java
@@ -33,7 +33,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  * 
  * @author Jason Bumgarner, Jason Shrewsbury
  * @version June 2003
- **/
+ */
 public class Symbol {
 	private String name;
 	private int address;
@@ -47,7 +47,7 @@ public class Symbol {
 	 * @param name    The name of the Symbol.
 	 * @param address The memory address that the Symbol refers to.
 	 * @param data    The type of Symbol that it is.
-	 **/
+	 */
 	public Symbol(String name, int address, boolean data) {
 		this.name = name;
 		this.address = address;
@@ -58,7 +58,7 @@ public class Symbol {
 	 * Returns the address of the the Symbol.
 	 * 
 	 * @return The address of the Symbol.
-	 **/
+	 */
 	public int getAddress() {
 		return this.address;
 	}
@@ -67,7 +67,7 @@ public class Symbol {
 	 * Returns the label of the the Symbol.
 	 * 
 	 * @return The label of the Symbol.
-	 **/
+	 */
 	public String getName() {
 		return this.name;
 	}
@@ -76,8 +76,7 @@ public class Symbol {
 	 * Finds the type of symbol, text or data.
 	 * 
 	 * @return The type of the data.
-	 **/
-
+	 */
 	public boolean getType() {
 		return this.data;
 	}
@@ -86,9 +85,8 @@ public class Symbol {
 	 * Sets (replaces) the address of the the Symbol.
 	 * 
 	 * @param newAddress The revised address of the Symbol.
-	 **/
+	 */
 	public void setAddress(int newAddress) {
 		this.address = newAddress;
-		return;
 	}
 }
