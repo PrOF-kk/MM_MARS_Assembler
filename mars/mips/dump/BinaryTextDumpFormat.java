5acf3e7a3a2bafb1d8d3da83eb27bcb370df0e68
PrOF-kk <valerio.colella.2001@gmail.com>
2021-06-07 11:55:29 +0200

    BinaryTextDumpFormat: Code refactor

--- mars/mips/dump/BinaryTextDumpFormat.java
+++ mars/mips/dump/BinaryTextDumpFormat.java
@@ -38,17 +38,14 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  * @author Pete Sanderson 
  * @version December 2007
  */
-
-
 public class BinaryTextDumpFormat extends AbstractDumpFormat {
 
 	/**
-	 *  Constructor.  There is no standard file extension for this format.
+	 * Constructor. There is no standard file extension for this format.
 	 */
 	public BinaryTextDumpFormat() {
 		super("Binary Text", "BinaryText", "Written as '0' and '1' characters to text file", null);
 	}
-
 
 	/**
 	 *  Write MIPS memory contents in binary text format.  Each line of
@@ -64,24 +61,24 @@ public class BinaryTextDumpFormat extends AbstractDumpFormat {
 	 *  @throws AddressErrorException if firstAddress is invalid or not on a word boundary.
 	 *  @throws IOException if error occurs during file output.
 	 */
-	public void dumpMemoryRange(File file, int firstAddress, int lastAddress) 
+	public void dumpMemoryRange(File file, int firstAddress, int lastAddress)
 			throws AddressErrorException, IOException {
-		PrintStream out = new PrintStream(new FileOutputStream(file));
-		String string = null;
-		try {
+
+		try (PrintStream out = new PrintStream(new FileOutputStream(file))) {
+
+			String string = null;
 			for (int address = firstAddress; address <= lastAddress; address += Memory.WORD_LENGTH_BYTES) {
+
 				Integer temp = Globals.memory.getRawWordOrNull(address);
-				if (temp == null) 
+				if (temp == null) {
 					break;
+				}
 				string = Integer.toBinaryString(temp.intValue());
 				while (string.length() < 32) {
 					string = '0' + string;
 				}
 				out.println(string);
 			}
-		} 
-		finally { 
-			out.close(); 
 		}
 	}
 
